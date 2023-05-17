87cc7fb6f2da49a0c2809a22e9c556d19019e589
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-18 00:42:17 +0300

    LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""  Basic Cache """
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ Class that inherits from BaseCaching and is a caching system
+        This caching system doesn’t have limit """
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        self.key_list = []
+
+    def put(self, key, item):
+        """ Assign to the dictionary """
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            self.key_list = list(self.cache_data.keys())
+            hold = self.key_list
+            self.key_list = hold[-BaseCaching.MAX_ITEMS:] + hold[
+                :-BaseCaching.MAX_ITEMS]
+            self.cache_data.pop(self.key_list[-1])
+            print(f"DISCARD: {self.key_list[-1]}")
+
+    def get(self, key):
+        """ Return the value linked to key"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        hold = self.key_list
+        self.key_list = hold[-BaseCaching.MAX_ITEMS:] + hold[
+            :-BaseCaching.MAX_ITEMS]
+        return self.cache_data.get(key)
