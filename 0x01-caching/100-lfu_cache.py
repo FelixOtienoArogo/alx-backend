3f650c34c90acb1f180bdacf0c7f67b6eb8a3eff
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-18 21:15:50 +0300

    ex

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -13,29 +13,30 @@ class LFUCache(BaseCaching):
 
     def put(self, key, item):
         """ Assign to the dictionary """
-        if key in self.lru_cache:
-            del self.lru_cache[key]
-        if len(self.lru_cache) > BaseCaching.MAX_ITEMS - 1:
-            min_value = min(self.lfu_cache.values())
-            lfu_keys = [k for k, v in self.lfu_cache.items() if v == min_value]
-            if len(lfu_keys) == 1:
-                print("DISCARD:", lfu_keys[0])
-                self.lru_cache.pop(lfu_keys[0])
-                del self.lfu_cache[lfu_keys[0]]
+        if key is not None and item is not None:
+            if key in self.lru_cache:
+                del self.lru_cache[key]
+            if len(self.lru_cache) > BaseCaching.MAX_ITEMS - 1:
+                min_value = min(self.lfu_cache.values())
+                lfu_keys = [k for k, v in self.lfu_cache.items() if v == min_value]
+                if len(lfu_keys) == 1:
+                    print("DISCARD:", lfu_keys[0])
+                    self.lru_cache.pop(lfu_keys[0])
+                    del self.lfu_cache[lfu_keys[0]]
+                else:
+                    for k, _ in list(self.lru_cache.items()):
+                        if k in lfu_keys:
+                            print("DISCARD:", k)
+                            self.lru_cache.pop(k)
+                            del self.lfu_cache[k]
+                            break
+            self.lru_cache[key] = item
+            self.lru_cache.move_to_end(key)
+            if key in self.lfu_cache:
+                self.lfu_cache[key] += 1
             else:
-                for k, _ in list(self.lru_cache.items()):
-                    if k in lfu_keys:
-                        print("DISCARD:", k)
-                        self.lru_cache.pop(k)
-                        del self.lfu_cache[k]
-                        break
-        self.lru_cache[key] = item
-        self.lru_cache.move_to_end(key)
-        if key in self.lfu_cache:
-            self.lfu_cache[key] += 1
-        else:
-            self.lfu_cache[key] = 1
-        self.cache_data = dict(self.lru_cache)
+                self.lfu_cache[key] = 1
+            self.cache_data = dict(self.lru_cache)
 
     def get(self, key):
         """ Return the value """
