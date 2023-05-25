7fd5af045ed12f500e36ae9a13f51f35052c873c
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-25 08:41:19 +0300

    l

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,41 +1,36 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
+""" Basic Flask app, Basic Babel setup, Get locale from request,
+    Parametrize templates """
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext
-from typing import Tuple
+
+app = Flask(__name__)
+babel = Babel(app)
+""" instantiate the Babel object """
 
 
-class Config:
-    """Configure available languages."""
-
-    LANGUAGES: Tuple[str] = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE: str = "en"
-    BABEL_DEFAULT_TIMEZONE: str = "UTC"
-
-
-app: Flask = Flask(__name__)
+class Config(object):
+    """ config class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
-"""Use that class as config for Flask app."""
-
-
-babel: Babel = Babel(app)
-
-
-@babel.localeselector
-def get_locale() -> str:
-    """Determine the best match with our supported languages."""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+""" Use that class as config for Flask app """
 
 
 @app.route('/')
-def hello() -> str:
-    """Just a test function."""
-    return render_template('3-index.html')
+def root():
+    """ basic Flask app """
+    return render_template("3-index.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@babel.localeselector
+def get_locale():
+    """ to determine the best match with our supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run()
